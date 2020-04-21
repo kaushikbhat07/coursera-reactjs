dfcdcdeabff8f27681d5db2acc468ddbbfdba929
Kaushik Bhat <kaushik.bantval98@gmail.com>
2020-04-21 23:54:48 +0530

    Single Page Application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,12 @@ import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -11,8 +17,10 @@ class Main extends Component {
 		super(props);
 		this.state = {
 			dishes: DISHES,
-			selectedDish: null
-		};
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS
+		  };
 	}
 
 	onDishSelect(dishId) {
@@ -20,11 +28,27 @@ class Main extends Component {
 	}
 
 	render() {
+		const HomePage = () => {
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+					leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+				/>
+			);
+		}
+
 		return (
 			<div>
 				<Header />
-				<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-				<DishDetail detail={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+				<Switch>
+					<Route path='/home' component={HomePage} />
+					<Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+					<Route exact path='/contactus' component={Contact} />} />
+					<Redirect to="/home" />
+				</Switch>
+				{/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> */}
+				{/* <DishDetail detail={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
 				<Footer />
 			</div>
 		);
